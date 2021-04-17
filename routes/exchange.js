f112df574bc86df6f04dafc4710febf98a890dcd
Alex <boyfun760@gmail.com>
2021-04-17 23:18:26 +0300

    exchange

--- routes/exchange.js
+++ routes/exchange.js
@@ -7,17 +7,48 @@ var User = require('../models/users');
 var func = require('../functions');
 var bot = require('../bot/bot');
 var Quiz = require('../models/quiz');
+const exchange = require('../models/exchange');
+const user = require('../../../fishingBot/models/user');
 
 
-router.post('/create', (req,res)=>{
+router.post('/create', (req,resp)=>{
     let from = req.body.from;
     let to = req.body.to;
     let artefact_from = req.body.artefact_from;
     let artefact_to = req.body.artefact_to;
 
     Exchange.create({from: from, to: to, artefact_from: artefact_from, artefact_to: artefact_to}).then(exchange=>{
-        res.statusCode = 200;
-        res.json({status: true});
+
+        User.find({login:{$in:[from,to]}}).then(res=>{
+            user1 = res[0];
+            user2 = res[1];
+
+            if(user1.login == to ){
+                var user_to = user1
+                var user_from = user2
+            } else {
+                var user_from = user1
+                var user_to = user2
+            }
+
+            var from_artefact;
+
+            user_from.artefacts.forEach((el,index)=>{
+                if(el._id == artefact_from){
+                    from_artefact = el.name;
+                }
+            })
+
+            user_to.artefacts.forEach((el,index)=>{
+                if(el._id == artefact_to){
+                    bot.sendMessage(user_to.chat_id, `Сокурсник @${from} хочет обменять ${from_artefact} на ${el.name}. Ответьте на предложение.` )
+                }
+            })
+
+            resp.statusCode = 200;
+            resp.json({status: true,exchange: exchange});
+        })
+    
     });
 
 
@@ -28,6 +59,7 @@ router.post('/submit', (req,res)=>{
     let accepter = req.body.login;
 
     Exchange.find({_id: exchange_id}).then(exchange=>{
+        if(exchange[0].status != 'ended'){
         if(accepter == exchange[0].to){
             exchange[0].status = "answering";
                 exchange[0].save().then(()=>{
@@ -43,11 +75,13 @@ router.post('/submit', (req,res)=>{
                         let keyboard = {
                             reply_markup: JSON.stringify({
                               inline_keyboard: [
-                                [{ text: answers[0], callback_data: {wxchange_id: exchange[0]._id, right_answer: answers[right_answer]} },{ text: answers[1], callback_data: {wxchange_id: exchange[0]._id, right_answer: answers[right_answer]} }],
-                                [{ text: answers[2], callback_data: {wxchange_id: exchange[0]._id, right_answer: answers[right_answer]} }, { text: answers[3], callback_data: {wxchange_id: exchange[0]._id, right_answer: answers[right_answer]} }]
+                                [{ text: answers[0], callback_data: `${right_answer}:${exchange[0]._id}:0`} ,{ text: answers[1], callback_data:  `${right_answer}:${exchange[0]._id}:1`} ],
+                                [{ text: answers[2], callback_data:  `${right_answer}:${exchange[0]._id}:2` }, { text: answers[3], callback_data:  `${right_answer}:${exchange[0]._id}:3` }]
                               ]
                             })
                           };
+
+                          
 
                           User.find({login: accepter}).then(u=>{
                               let user = u[0];
@@ -60,10 +94,17 @@ router.post('/submit', (req,res)=>{
                                   bot.sendMessage(user.chat_id,question,keyboard);
                               })
 
+                              res.statusCode = 200;
+                              res.json({status: "answering"})
+
                           })
                     })
                 })
         }
+    } else {
+        res.statusCode = 400;
+        res.json({err: "Up to date"})
+    }
     })
 })
 
@@ -75,12 +116,118 @@ router.post('/submit', (req,res)=>{
 
             if(exchange == undefined){
                 res.statusCode = 200;
-                res.json({status: 'doesnt have pending exchanges', exchange: {}})
+                res.json({status: 'user doesnt have pending exchanges', exchange: {}})
             } else {
                 res.statusCode = 200;
                 res.json({status: true, exchange: exchange });
             }
         })
+    })
+
+    bot.on('callback_query',(msg)=>{
+        
+        var chatId = msg.from.id;
+        var username = msg.from.username;
+
+        var right_answer = msg.data.split(':')[0];
+        var exchange_id = msg.data.split(':')[1];
+        var answer = msg.data.split(':')[2];
+
+        if(answer == right_answer){
+            bot.sendMessage(chatId,'🥳Вы ответили верно!');
+            Exchange.find({_id: exchange_id}).then(ex=>{
+                let exchange = ex[0];
+                if(exchange.status != 'ended'){
+                    
+                    if(username == exchange.from){
+
+                        User.find({login: exchange.to}).then(u=>{
+                            let user = u[0];
+                            bot.sendMessage(user.chat_id, '🥳Ваш сокурсник ответил верно!');
+
+                            exchange.answered_from = true;
+
+                            if(exchange.answered_to){
+
+
+                                bot.sendMessage(chatId,'Обмен прошел успешно!');
+                                bot.sendMessage(user.chat_id,'Обмен прошел успешно!')
+
+                                exchange.status = 'ended';
+                            }
+                            
+                            exchange.save().then(()=>{
+                            
+                            })
+                        })
+
+                    
+                    } else {
+                        User.find({login: exchange.from}).then(u=>{
+                            let user = u[0];
+                            bot.sendMessage(user.chat_id, '🥳Ваш сокурсник ответил верно!');
+
+                            exchange.answered_to = true;
+
+                        if(exchange.answered_from){
+                            bot.sendMessage(chatId,'Обмен прошел успешно!');
+                            bot.sendMessage(user.chat_id,'Обмен прошел успешно!')
+
+                            exchange.status = 'ended';
+                        }
+
+                        
+                        exchange.save().then(()=>{
+                        
+                        })
+                        })
+
+                        
+                    }
+                
+                } else {
+                    bot.sendMessage(chatId,'Вы уже отвечали на этот вопрос!')
+                }
+            })
+        } else {
+            bot.sendMessage(chatId, '🤦‍♂️Вы ответили неверно!');
+            Exchange.find({_id: exchange_id}).then(e=>{
+                
+                let exchange = e[0];
+
+                if(exchange.status != 'ended'){
+
+                    if(exchange.from == username){
+                        User.find({login: exchange.to}).then((u)=>{
+                            let user = u[0];
+                            bot.sendMessage(user.chat_id, 'Ваш сокурсник ответил неверно. Обмен запрещен!')
+                        })
+                        exchange.status = 'ended';
+                        exchange.save(()=>{
+
+                        })
+                    } else {
+                        User.find({login: exchange.from}).then((u)=>{
+                            let user = u[0];
+                            bot.sendMessage(user.chat_id, 'Ваш сокурсник ответил неверно. Обмен запрещен!')
+                        })
+                        exchange.status = 'ended';
+                        exchange.save(()=>{
+                            
+                        })
+                    }
+
+                } else {
+                    bot.sendMessage(chatId,'Вы уже отвечали на этот вопрос!')
+                }
+            })
+
+        }
+
+
+
+        // console.log(msg)
+
     })
 
 
