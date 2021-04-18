0f586cb0a1f5900bb1fa546a073393f365b6cd24
GentleGame <banderes94@gmail.com>
2021-04-18 11:56:13 +0300

    fix heroku 7

--- client/src/store.js
+++ client/src/store.js
@@ -34,7 +34,7 @@ class Store{
     }
     @action.bound
     sendTg(tg){
-        axios.post(`${serverUrl}/users/login`, {login: tg}).then((res) => {
+        axios.post(`${serverUrl}/users/login`, {login: '@'+tg}).then((res) => {
         if(res.data.auth == 'pending'){
             this.setLogin(tg.substr(1))
         } else if(res.data.auth){
